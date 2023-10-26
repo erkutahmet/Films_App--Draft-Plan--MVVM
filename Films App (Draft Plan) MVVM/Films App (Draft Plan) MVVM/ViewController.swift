024b293a55a73f096c138af4311b297ecc478ef1
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-26 03:24:59 +0300

    created Films App (Draft Plan) MVVM xcode project

---
+++ Films App (Draft Plan) MVVM/Films App (Draft Plan) MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Films App (Draft Plan) MVVM
+//
+//  Created by Ahmet Erkut on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
